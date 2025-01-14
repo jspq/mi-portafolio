12401899f914d56285699e0ba6ced679efafb7db
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-14 16:38:26 -0500

    Adds About page to portfolio
    Adds a new About page to the portfolio website, displaying basic information.  The About page component, along with its associated files (HTML, CSS, and tests), has been created and integrated into the main portfolio component.

--- src/app/components/mi-portafolio/mi-portafolio.component.ts
+++ src/app/components/mi-portafolio/mi-portafolio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
 import { HomeComponent } from '../pages/home/home.component';
+import { AboutComponent } from '../pages/about/about.component';
 
 @Component({
   selector: 'mi-portafolio',
   standalone: true,
-  imports: [NavComponent, FooterComponent, HomeComponent],
+  imports: [NavComponent, FooterComponent, HomeComponent, AboutComponent],
   templateUrl: './mi-portafolio.component.html',
   styleUrl: './mi-portafolio.component.css'
 })
