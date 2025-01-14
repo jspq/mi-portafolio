53d8547e57daa0f992a75482734fdddc4d350d98
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-14 13:17:49 -0500

    Adds application navigation bar
    Implements a material design navigation bar component to the application, enhancing user experience and providing intuitive access to key app features.  Includes menu, favorite, and share icons.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+/**
+ * @title Basaic sidenav
+ */
 @Component({
   selector: 'app-nav',
-  standalone: true,
-  imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class NavComponent {
 
