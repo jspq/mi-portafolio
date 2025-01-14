750c9870c2d0c6b342d818bbf06b334a7fbb931d
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-14 16:30:22 -0500

    Adds home page component
    Introduces a dedicated home page component to improve the structure and organization of the portfolio website.  The home page is now displayed as the main content on the portfolio's main page.

--- src/app/components/mi-portafolio/mi-portafolio.component.ts
+++ src/app/components/mi-portafolio/mi-portafolio.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
+import { HomeComponent } from '../pages/home/home.component';
 
 @Component({
   selector: 'mi-portafolio',
   standalone: true,
-  imports: [NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent, HomeComponent],
   templateUrl: './mi-portafolio.component.html',
   styleUrl: './mi-portafolio.component.css'
 })
