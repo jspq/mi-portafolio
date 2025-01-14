c3eb12d5cf6eea5f7675ea7efbbd50c965349b3b
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-14 16:40:30 -0500

    Adds experience section to portfolio
    Adds a new component to display professional experience.  Updates the main portfolio component to include the new experience section in the layout.

--- src/app/components/mi-portafolio/mi-portafolio.component.ts
+++ src/app/components/mi-portafolio/mi-portafolio.component.ts
@@ -3,11 +3,18 @@ import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { AboutComponent } from '../pages/about/about.component';
+import { ExperienceComponent } from '../pages/experience/experience.component';
 
 @Component({
   selector: 'mi-portafolio',
   standalone: true,
-  imports: [NavComponent, FooterComponent, HomeComponent, AboutComponent],
+  imports: [
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent
+  ],
   templateUrl: './mi-portafolio.component.html',
   styleUrl: './mi-portafolio.component.css'
 })
