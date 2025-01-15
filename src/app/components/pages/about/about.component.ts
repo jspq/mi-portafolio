b3ba3f4c818161c3e4ce4f8b0eb26dcf73ceaa6b
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-15 10:43:30 -0500

    Improves About page layout
    Uses a grid layout to improve the visual presentation of the About page content.  Adds Angular Material's grid list component for better responsiveness.

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
