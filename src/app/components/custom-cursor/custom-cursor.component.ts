59f1f4503600ec6dbd9f2baecd7cfb24db61b18d
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-17 21:31:24 -0500

    Implements custom cursor component
    Adds a visually engaging custom cursor with interactive elements that respond to hover and click events.  The cursor includes a central dot and a surrounding ring, both animating on hover and click.  The cursor's appearance adapts based on the element being hovered. Improves overall user experience.

--- src/app/components/custom-cursor/custom-cursor.component.ts
+++ src/app/components/custom-cursor/custom-cursor.component.ts
@@ -1,20 +1,38 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-custom-cursor',
   standalone: true,
-  imports: [],
   templateUrl: './custom-cursor.component.html',
-  styleUrl: './custom-cursor.component.css'
+  styleUrl: './custom-cursor.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomCursorComponent {
   cursorX: number = 0;
   cursorY: number = 0;
+  isClicking: boolean = false;
+  isMoving: boolean = false;
+  moveTimeout: any;
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
-    this.cursorX = e.clientX - 10;
-    this.cursorY = e.clientY - 10;
+    this.cursorX = e.clientX;
+    this.cursorY = e.clientY;
+
+    this.isMoving = true;
+    clearTimeout(this.moveTimeout);
+    this.moveTimeout = setTimeout(() => {
+      this.isMoving = false;
+    }, 100);
   }
 
+  @HostListener('document:mousedown')
+  onMouseDown() {
+    this.isClicking = true;
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    this.isClicking = false;
+  }
 }
