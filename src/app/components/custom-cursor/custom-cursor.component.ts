0dbdd570a50f9f7b29f7ee499ad656d85340bae7
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-17 21:31:30 -0500

    Refactors custom cursor logic
    Removes unnecessary click and move tracking from the custom cursor component, simplifying its implementation and improving performance.  The cursor now only tracks mouse position.

--- src/app/components/custom-cursor/custom-cursor.component.ts
+++ src/app/components/custom-cursor/custom-cursor.component.ts
@@ -10,29 +10,10 @@ import { Component, HostListener, ChangeDetectionStrategy } from '@angular/core'
 export class CustomCursorComponent {
   cursorX: number = 0;
   cursorY: number = 0;
-  isClicking: boolean = false;
-  isMoving: boolean = false;
-  moveTimeout: any;
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
     this.cursorX = e.clientX;
     this.cursorY = e.clientY;
-
-    this.isMoving = true;
-    clearTimeout(this.moveTimeout);
-    this.moveTimeout = setTimeout(() => {
-      this.isMoving = false;
-    }, 100);
-  }
-
-  @HostListener('document:mousedown')
-  onMouseDown() {
-    this.isClicking = true;
-  }
-
-  @HostListener('document:mouseup')
-  onMouseUp() {
-    this.isClicking = false;
   }
 }
