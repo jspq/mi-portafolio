30fd541d26b027d60a5e7aa6fe3420edf46bf072
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-22 16:01:52 -0500

    Adds expandable experience section
    Improves the experience page by adding expandable panels to display information in a more organized and engaging way.  Adds Angular Material's expansion panels to the component.

--- src/app/components/pages/experience/experience.component.ts
+++ src/app/components/pages/experience/experience.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatExpansionModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
-
+  panelOpenState: boolean = false;
 }
