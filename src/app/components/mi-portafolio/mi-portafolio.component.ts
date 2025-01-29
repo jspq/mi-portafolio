84f34651b5fb331fc9b8c968e582e83fe7d9f131
jspq <juansebastianpq812@gmail.com>
2025-01-29 12:10:04 -0500

    Adds a contacts section
    Adds a new component to display contact information and links to social media and communication channels.  Includes styling for buttons and links.

--- src/app/components/mi-portafolio/mi-portafolio.component.ts
+++ src/app/components/mi-portafolio/mi-portafolio.component.ts
@@ -3,6 +3,9 @@ import { NavComponent } from '../nav/nav.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ScrollToTopComponent, CustomCursorComponent } from '../index';
 import { HomeComponent, AboutComponent, ExperienceComponent } from '../pages/index';
+import { ServiceComponent } from '../pages/service/service.component';
+import { PortfolioComponent } from '../pages/portfolio/portfolio.component';
+import { ContactsComponent } from '../pages/contacts/contacts.component';
 
 @Component({
   selector: 'mi-portafolio',
@@ -14,7 +17,11 @@ import { HomeComponent, AboutComponent, ExperienceComponent } from '../pages/ind
     AboutComponent,
     ExperienceComponent,
     ScrollToTopComponent,
-    CustomCursorComponent
+    CustomCursorComponent,
+    ServiceComponent,
+    PortfolioComponent,
+    FooterComponent,
+    ContactsComponent
   ],
   templateUrl: './mi-portafolio.component.html',
   styleUrl: './mi-portafolio.component.css'
---
+++ src/app/components/pages/contacts/contacts.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contacts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css'
+})
+export class ContactsComponent {
+  phoneNumber = '+57 3143001234';
+  whatsappLink = `https://wa.me/${this.phoneNumber}`;
+  telegramLink = 'https://www.telegram.com/usuario';
+  instagramLink = 'https://www.instagram.com/usuario';
+  linkedinLink = 'https://www.linkedin.com/in/usuario';
+  phoneCallLink = `tel:${this.phoneNumber}`;
+}
