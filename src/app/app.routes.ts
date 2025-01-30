70d13e27c8ff1d66206442f8dd8b7e49d648193e
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-01-29 21:29:18 -0500

    Adds basic page structure and navigation
    Introduces a main layout with sections for content, a scroll-to-top button, and a custom cursor.  Updates routing to include a default route for the portfolio.  Improves overall application structure and user experience.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MiPortafolioComponent } from './components/mi-portafolio/mi-portafolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MiPortafolioComponent, title: 'Mi Portafolio' }
+];
