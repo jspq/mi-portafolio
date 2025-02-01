28dae468b33d1ec78ff3edcdf0ebdcf7ce365ff5
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-01 15:31:09 -0500

    Migrates stylesheets to SCSS
    Converts all CSS files to SCSS, improving maintainability and styling capabilities.  Updates Angular project configuration to use SCSS for new components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,19 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MiPortafolioComponent } from './components/mi-portafolio/mi-portafolio.component';
 import { ThemeService } from './services';
-import { ScrollToTopComponent, CustomCursorComponent } from "./components";
+import { ScrollToTopComponent, CustomCursorComponent } from './components';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        RouterModule,
-        MiPortafolioComponent,
-        ScrollToTopComponent,
-        CustomCursorComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [
+    RouterModule,
+    MiPortafolioComponent,
+    ScrollToTopComponent,
+    CustomCursorComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
-  constructor(public themeService: ThemeService) { }
+  constructor(public themeService: ThemeService) {}
 }
