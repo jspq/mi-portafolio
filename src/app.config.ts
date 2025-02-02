7b1d805db1ce74689b9d6b1c2f64692dff543d1b
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-01 21:06:08 -0500

    Moves application configuration
    Relocates the application configuration file to the root `src` directory for better organization and improves the import statement in the `main.ts` file.

---
+++ src/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+        provideAnimationsAsync(),
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+    ]
+};
