fa26ac25105910b94eb111090eeeb4d5ac140662
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-03 00:16:14 -0500

    Updates application routing
    Adds a landing page and redirects to a 404 page for unknown routes.  Improves the overall user experience by providing a clearer navigation structure.

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { Landing } from './app/pages/landing/landing';
 import { MiPortafolioComponent } from './app/components/mi-portafolio/mi-portafolio.component';
 
-export const appRoutes: Routes = [{ path: '', component: MiPortafolioComponent, title: 'Mi Portafolio' }];
+export const appRoutes: Routes = [
+    { path: 'portafolio', component: MiPortafolioComponent, title: 'Mi Portafolio' },
+    { path: '', component: Landing, title: 'Mi Portafolio' },
+    { path: '**', redirectTo: '/notfound' }
+];
