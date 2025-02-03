72634be4d2671810693a5e9ff1cde0d1cd91daa8
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-03 00:22:34 -0500

    Moves footer component
    Relocates the footer component to improve project structure and maintainability.

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,5 @@
 export * from './nav/nav.component';
-export * from './footer/footer.component';
+export * from '../pages/landing/components/footerwidget';
 export * from './scroll-to-top/scroll-to-top.component';
 export * from './custom-cursor/custom-cursor.component';
 export * from './theme-toggle/theme-toggle.component';
