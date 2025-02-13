d5d80de7d7d1f1644f0584a3ea8e3dec3e1969c7
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-13 10:37:48 -0500

    Removes portafolio route
    Removes the route to the portafolio component.  The landing page now serves as the default view.

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Landing } from './app/pages/landing/landing';
-import { MiPortafolioComponent } from './app/components/mi-portafolio/mi-portafolio.component';
+// import { MiPortafolioComponent } from './app/components/mi-portafolio/mi-portafolio.component';
 
 export const appRoutes: Routes = [
-    { path: 'portafolio', component: MiPortafolioComponent, title: 'Mi Portafolio' },
+    // { path: 'portafolio', component: MiPortafolioComponent, title: 'Mi Portafolio' },
     { path: '', component: Landing, title: 'Mi Portafolio' },
     { path: '**', redirectTo: '/notfound' }
 ];
